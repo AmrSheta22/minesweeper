f2d9c94d38ea813b245445fed064dc0bbe2d836e
Amr Sheta <amrsheta751@gmail.com>
2023-03-03 22:55:12 +0200

    add game at terminal and game logic

---
+++ game.py
@@ -0,0 +1,175 @@
+from logic import *
+
+# make a numpy array with all elements set to "*"
+OPENED= []
+def make_hidden_grid(height, width):
+    grid = np.full((height, width), "*")
+    return grid
+def check_element(grid, spot):
+    upper_left = grid[spot[0]-1, spot[1]-1]
+    upper = grid[spot[0]-1, spot[1]]
+    upper_right = grid[spot[0]-1, spot[1]+1]
+    left = grid[spot[0], spot[1]-1]
+    right = grid[spot[0], spot[1]+1]
+    lower_left = grid[spot[0]+1, spot[1]-1]
+    lower = grid[spot[0]+1, spot[1]]
+    lower_right = grid[spot[0]+1, spot[1]+1]
+    around = [(upper_left, (spot[0]-1, spot[1]-1)), (upper, (spot[0]-1, spot[1])), (upper_right, (spot[0]-1, spot[1]+1)), (left, (spot[0], spot[1]-1)), (right, (spot[0], spot[1]+1)), (lower_left, (spot[0]+1, spot[1]-1)), (lower, (spot[0]+1, spot[1])), (lower_right, (spot[0]+1, spot[1]+1))]  
+    return around
+
+
+def middle_click(spot, user_view, hidden_grid):
+    hidden_grid[spot[0]-1, spot[1]-1] = str(user_view[spot[0]-1, spot[1]-1] )
+    hidden_grid[spot[0]-1, spot[1]] = str(user_view[spot[0]-1, spot[1]])
+    hidden_grid[spot[0]-1, spot[1]+1]= str(user_view[spot[0]-1, spot[1]+1])
+    hidden_grid[spot[0], spot[1]-1] = str(user_view[spot[0], spot[1]-1] )
+    hidden_grid[spot[0], spot[1]+1] = str(user_view[spot[0], spot[1]+1])
+    hidden_grid[spot[0]+1, spot[1]-1] = str(user_view[spot[0]+1, spot[1]-1]) 
+    hidden_grid[spot[0]+1, spot[1]] = str(user_view[spot[0]+1, spot[1]])
+    hidden_grid[spot[0]+1, spot[1]+1] = str(user_view[spot[0]+1, spot[1]+1])
+    return hidden_grid
+def edge_click(spot, user_view, hidden_grid):
+    if is_it_edge(user_view, spot)[1] == "top":
+        hidden_grid[spot[0], spot[1]-1] = str(user_view[spot[0], spot[1]-1] )
+        hidden_grid[spot[0], spot[1]+1] = str(user_view[spot[0], spot[1]+1])
+        hidden_grid[spot[0]+1, spot[1]-1] = str(user_view[spot[0]+1, spot[1]-1]) 
+        hidden_grid[spot[0]+1, spot[1]] = str(user_view[spot[0]+1, spot[1]])
+        hidden_grid[spot[0]+1, spot[1]+1] = str(user_view[spot[0]+1, spot[1]+1])
+    elif is_it_edge(user_view, spot)[1] == "bottom":
+        hidden_grid[spot[0]-1, spot[1]-1] = str(user_view[spot[0]-1, spot[1]-1] )
+        hidden_grid[spot[0]-1, spot[1]] = str(user_view[spot[0]-1, spot[1]])
+        hidden_grid[spot[0]-1, spot[1]+1]= str(user_view[spot[0]-1, spot[1]+1])
+        hidden_grid[spot[0], spot[1]-1] = str(user_view[spot[0], spot[1]-1] )
+        hidden_grid[spot[0], spot[1]+1] = str(user_view[spot[0], spot[1]+1])
+    elif is_it_edge(user_view, spot)[1] == "left":
+        hidden_grid[spot[0]-1, spot[1]] = str(user_view[spot[0]-1, spot[1]])
+        hidden_grid[spot[0]-1, spot[1]+1]= str(user_view[spot[0]-1, spot[1]+1])
+        hidden_grid[spot[0], spot[1]+1] = str(user_view[spot[0], spot[1]+1])
+        hidden_grid[spot[0]+1, spot[1]] = str(user_view[spot[0]+1, spot[1]])
+        hidden_grid[spot[0]+1, spot[1]+1] = str(user_view[spot[0]+1, spot[1]+1])
+    elif is_it_edge(user_view, spot)[1] == "right":
+        hidden_grid[spot[0]-1, spot[1]-1] = str(user_view[spot[0]-1, spot[1]-1] )
+        hidden_grid[spot[0]-1, spot[1]] = str(user_view[spot[0]-1, spot[1]])
+        hidden_grid[spot[0], spot[1]-1] = str(user_view[spot[0], spot[1]-1] )
+        hidden_grid[spot[0]+1, spot[1]-1] = str(user_view[spot[0]+1, spot[1]-1]) 
+        hidden_grid[spot[0]+1, spot[1]] = str(user_view[spot[0]+1, spot[1]])
+    return hidden_grid
+
+
+def corner_click(spot, user_view, hidden_grid):
+    if is_it_corner(user_view, spot)[1] == "top left":
+        hidden_grid[spot[0], spot[1]+1] = str(user_view[spot[0], spot[1]+1])
+        hidden_grid[spot[0]+1, spot[1]] = str(user_view[spot[0]+1, spot[1]])
+        hidden_grid[spot[0]+1, spot[1]+1] = str(user_view[spot[0]+1, spot[1]+1])
+    elif is_it_corner(user_view, spot)[1] == "top right":
+        hidden_grid[spot[0], spot[1]-1] = str(user_view[spot[0], spot[1]-1] )
+        hidden_grid[spot[0]+1, spot[1]-1] = str(user_view[spot[0]+1, spot[1]-1]) 
+        hidden_grid[spot[0]+1, spot[1]] = str(user_view[spot[0]+1, spot[1]])
+    elif is_it_corner(user_view, spot)[1] == "bottom left":
+        hidden_grid[spot[0]-1, spot[1]] = str(user_view[spot[0]-1, spot[1]])
+        hidden_grid[spot[0]-1, spot[1]+1]= str(user_view[spot[0]-1, spot[1]+1])
+        hidden_grid[spot[0], spot[1]+1] = str(user_view[spot[0], spot[1]+1])
+    elif is_it_corner(user_view, spot)[1] == "bottom right":
+        hidden_grid[spot[0]-1, spot[1]-1] = str(user_view[spot[0]-1, spot[1]-1] )
+        hidden_grid[spot[0]-1, spot[1]] = str(user_view[spot[0]-1, spot[1]])
+        hidden_grid[spot[0], spot[1]-1] = str(user_view[spot[0], spot[1]-1] )
+    return hidden_grid
+
+def around_edge(user_view, spot):
+    around = []
+    if is_it_edge(user_view, spot)[1] == "top":
+        around.append((user_view[spot[0], spot[1]-1], (spot[0], spot[1]-1)))
+        around.append((user_view[spot[0], spot[1]+1], (spot[0], spot[1]+1)))
+        around.append((user_view[spot[0]+1, spot[1]-1], (spot[0]+1, spot[1]-1)))
+        around.append((user_view[spot[0]+1, spot[1]], (spot[0]+1, spot[1])))
+        around.append((user_view[spot[0]+1, spot[1]+1], (spot[0]+1, spot[1]+1)))
+    elif is_it_edge(user_view, spot)[1] == "bottom":
+        around.append((user_view[spot[0]-1, spot[1]-1], (spot[0]-1, spot[1]-1)))
+        around.append((user_view[spot[0]-1, spot[1]], (spot[0]-1, spot[1])))
+        around.append((user_view[spot[0]-1, spot[1]+1], (spot[0]-1, spot[1]+1)))
+        around.append((user_view[spot[0], spot[1]-1], (spot[0], spot[1]-1)))
+        around.append((user_view[spot[0], spot[1]+1], (spot[0], spot[1]+1)))
+    elif is_it_edge(user_view, spot)[1] == "left":
+        around.append((user_view[spot[0]-1, spot[1]], (spot[0]-1, spot[1])))
+        around.append((user_view[spot[0]-1, spot[1]+1], (spot[0]-1, spot[1]+1)))
+        around.append((user_view[spot[0], spot[1]+1], (spot[0], spot[1]+1)))
+        around.append((user_view[spot[0]+1, spot[1]], (spot[0]+1, spot[1])))
+        around.append((user_view[spot[0]+1, spot[1]+1], (spot[0]+1, spot[1]+1)))
+    elif is_it_edge(user_view, spot)[1] == "right":
+        around.append((user_view[spot[0]-1, spot[1]-1], (spot[0]-1, spot[1]-1)))
+        around.append((user_view[spot[0]-1, spot[1]], (spot[0]-1, spot[1])))
+        around.append((user_view[spot[0], spot[1]-1], (spot[0], spot[1]-1)))
+        around.append((user_view[spot[0]+1, spot[1]-1], (spot[0]+1, spot[1]-1)))
+        around.append((user_view[spot[0]+1, spot[1]], (spot[0]+1, spot[1])))
+    return around
+
+def around_corner(user_view, spot):
+    around = []
+    if is_it_corner(user_view, spot)[1] == "top left":
+        around.append((user_view[spot[0], spot[1]+1], (spot[0], spot[1]+1)))
+        around.append((user_view[spot[0]+1, spot[1]], (spot[0]+1, spot[1])))
+        around.append((user_view[spot[0]+1, spot[1]+1], (spot[0]+1, spot[1]+1)))
+    elif is_it_corner(user_view, spot)[1] == "top right":
+        around.append((user_view[spot[0], spot[1]-1], (spot[0], spot[1]-1)))
+        around.append((user_view[spot[0]+1, spot[1]-1], (spot[0]+1, spot[1]-1)))
+        around.append((user_view[spot[0]+1, spot[1]], (spot[0]+1, spot[1])))
+    elif is_it_corner(user_view, spot)[1] == "bottom left":
+        around.append((user_view[spot[0]-1, spot[1]], (spot[0]-1, spot[1])))
+        around.append((user_view[spot[0]-1, spot[1]+1], (spot[0]-1, spot[1]+1)))
+        around.append((user_view[spot[0], spot[1]+1], (spot[0], spot[1]+1)))
+    elif is_it_corner(user_view, spot)[1] == "bottom right":
+        around.append((user_view[spot[0]-1, spot[1]-1], (spot[0]-1, spot[1]-1)))
+        around.append((user_view[spot[0]-1, spot[1]], (spot[0]-1, spot[1])))
+        around.append((user_view[spot[0], spot[1]-1], (spot[0], spot[1]-1)))
+    return around
+
+def click(spot, user_view, hidden_grid):
+    if user_view[spot[0], spot[1]] == 0:
+        hidden_grid[spot[0], spot[1]] = "0"
+        OPENED.append(spot)
+        if is_it_corner(user_view, spot)[0]:
+            around = around_corner(user_view, spot)
+            hidden_grid = corner_click(spot, user_view, hidden_grid)
+        elif is_it_edge(user_view, spot)[0]:
+            around = around_edge(user_view, spot)
+            hidden_grid = edge_click(spot, user_view, hidden_grid)
+        else:
+            hidden_grid = middle_click(spot, user_view, hidden_grid)
+            around = check_element(user_view, spot)
+        for i in around:
+            if i[0] != 0 and i[1] not in OPENED:
+                OPENED.append(i[1])
+        for i in around:
+            if i[0] == 0 and i[1] not in OPENED:
+                OPENED.append(i[1])
+                click(i[1], user_view, hidden_grid)
+        return hidden_grid
+    elif user_view[spot[0], spot[1]] == -1:
+        return "You lost"
+    else:
+        hidden_grid[spot[0], spot[1]] = str(user_view[spot[0], spot[1]])
+        OPENED.append(spot)
+    return hidden_grid
+
+main_grid = grid(10, 10, .16)
+user_view = user_view_matrix(main_grid)
+hidden_grid = make_hidden_grid(10, 10)
+print("   0   1   2   3   4   5   6   7   8   9")
+print(hidden_grid)
+while True:
+    x = input("Enter x coordinate: ")
+    y = input("Enter y coordinate: ")
+    try:
+        spot = (int(x), int(y))
+    except:
+        print("Enter a number")
+        continue
+    if x == "q" or y == "q":
+        break
+    if int(x) > hidden_grid.shape[0] or int(y) > hidden_grid.shape[1]:
+        print("Enter a number within the grid")
+        continue
+    print("   0   1   2   3   4   5   6   7   8   9")
+    print(click(spot, user_view, hidden_grid))
+
+
---
+++ logic.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+def grid(height, width, bomb_density=.2):
+    x= np.random.choice([0,1], size=(height, width), p=[1-bomb_density, bomb_density])
+    return x
+
+def check_around(grid, spot):
+    upper_left = grid[spot[0]-1, spot[1]-1]
+    upper = grid[spot[0]-1, spot[1]]
+    upper_right = grid[spot[0]-1, spot[1]+1]
+    left = grid[spot[0], spot[1]-1]
+    right = grid[spot[0], spot[1]+1]
+    lower_left = grid[spot[0]+1, spot[1]-1]
+    lower = grid[spot[0]+1, spot[1]]
+    lower_right = grid[spot[0]+1, spot[1]+1]
+    return upper_left + upper + upper_right + left + right + lower_left + lower + lower_right
+
+def is_it_edge(grid, spot):
+    if spot[0] == 0:
+        return (True, "top")
+    elif spot[1] == 0:
+        return (True, "left")
+    elif spot[0]+1 == grid.shape[0]:
+        return (True, "bottom")
+    elif spot[1]+1 == grid.shape[1]:
+        return (True, "right")
+    else: 
+        return (False, "not an edge")
+    
+def is_it_corner(grid, spot):
+    if spot[0] ==0 and spot[1] == 0:
+        return (True, "top left")
+    elif spot[0]+1 == grid.shape[0] and spot[1] == 0:
+        return (True, "bottom left")
+    elif spot[0] == 0 and spot[1]+1 == grid.shape[1]:
+        return (True, "top right")
+    elif spot[0]+1 == grid.shape[0] and spot[1]+1 == grid.shape[1]:
+        return (True, "bottom right")
+    else: 
+        return (False, "not a corner")    
+
+def check_around_corner(grid, spot):
+    if is_it_corner(grid, spot)[1] == "top left":
+        right = grid[spot[0], spot[1]+1]
+        print(grid[spot[0], spot[1]])
+        lower = grid[spot[0]+1, spot[1]]
+        lower_right = grid[spot[0]+1, spot[1]+1]
+        return right + lower + lower_right
+    elif is_it_corner(grid, spot)[1] == "bottom left":
+        upper = grid[spot[0]-1, spot[1]]
+        upper_right = grid[spot[0]-1, spot[1]+1]
+        right = grid[spot[0], spot[1]+1]
+        return upper + upper_right + right
+    elif is_it_corner(grid, spot)[1] == "top right":
+        left = grid[spot[0], spot[1]-1]
+        lower_left = grid[spot[0]+1, spot[1]-1]
+        lower = grid[spot[0]+1, spot[1]]
+        return left + lower_left + lower
+    else :
+        upper_left = grid[spot[0]-1, spot[1]-1]
+        upper = grid[spot[0]-1, spot[1]]
+        left = grid[spot[0], spot[1]-1]
+        return upper_left + upper + left
+
+def check_around_edge(grid, spot):
+    if is_it_edge(grid, spot)[1] == "top":
+        left = grid[spot[0], spot[1]-1]
+        right = grid[spot[0], spot[1]+1]
+        lower_left = grid[spot[0]+1, spot[1]-1]
+        lower = grid[spot[0]+1, spot[1]]
+        lower_right = grid[spot[0]+1, spot[1]+1]
+        return left + right + lower_left + lower + lower_right
+    elif is_it_edge(grid, spot)[1] == "left":
+        upper = grid[spot[0]-1, spot[1]]
+        upper_right = grid[spot[0]-1, spot[1]+1]
+        right = grid[spot[0], spot[1]+1]
+        lower = grid[spot[0]+1, spot[1]]
+        lower_right = grid[spot[0]+1, spot[1]+1]
+        return upper + upper_right + right + lower + lower_right
+    elif is_it_edge(grid, spot)[1] == "bottom":
+        upper_left = grid[spot[0]-1, spot[1]-1]
+        upper = grid[spot[0]-1, spot[1]]
+        left = grid[spot[0], spot[1]-1]
+        right = grid[spot[0], spot[1]+1]
+        upper_right = grid[spot[0]-1, spot[1]+1]
+        return upper_left + upper + left + right + upper_right
+    else:
+        upper_left = grid[spot[0]-1, spot[1]-1]
+        upper = grid[spot[0]-1, spot[1]]
+        lower = grid[spot[0]+1, spot[1]]
+        left = grid[spot[0], spot[1]-1]
+        lower_left = grid[spot[0]+1, spot[1]-1]
+        return upper_left + upper + lower + left + lower_left     
+
+def user_view_matrix(grid):
+    x = np.zeros(grid.shape)
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            if grid[i,j] == 1:
+                x[i,j] = -1
+            elif is_it_corner(grid, (i,j))[0]:
+                x[i,j] = check_around_corner(grid, (i,j))
+            elif is_it_edge(grid, (i,j))[0]:
+                x[i,j] = check_around_edge(grid, (i,j))
+            else:
+                x[i,j] = check_around(grid, (i,j))
+    return x
